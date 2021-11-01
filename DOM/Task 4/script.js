abab8bb5987f710fc587d4e9438f14a233162e77
TheHackerOne <yash2200gmail.com>
2021-11-01 12:48:59 +0530

    Style property in DOM

--- DOM/Task 4/script.js
+++ DOM/Task 4/script.js
@@ -1,4 +1,27 @@
 // style property DOM
 // Children -> return HTMLCollection
 
-// childNodes -> return nodeList
+// childNodes -> return nodeList -> forEach can be used here
+
+// All HTML elements are nodes but all nodes(such as comment) are not HTML elements
+
+let select = document.querySelector('select')
+let allMovies = document.querySelectorAll('.movies')
+
+select.addEventListener('change', (e) => {
+    let selectValue = select.value
+    if(selectValue === 'none'){
+        allMovies.forEach((movie) => {
+            movie.style.display = 'block'
+        })
+    }else{
+        allMovies.forEach((movie) => {
+            let category = movie.querySelector('p').getAttribute('data-category')
+            if(selectValue !== category){
+                movie.style.display = 'none'
+            }else{
+                movie.style.display = 'block'
+            }
+        })
+    }
+})
